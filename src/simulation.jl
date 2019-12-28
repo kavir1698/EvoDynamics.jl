8b024551af0d1f1009366b6afc409371b4dad1c4
Ali R. Vahdati <arv.kavir@gmail.com>
2019-12-28 18:27:20 +0100

    Add spatial structure

--- src/simulation.jl
+++ src/simulation.jl
@@ -4,7 +4,7 @@ A `struct` for individuals that keeps individual-specific variables.
 """
 mutable struct Ind{A<:Integer, B<:AbstractVector, D<:AbstractFloat, E<:AbstractArray} <: AbstractAgent
   id::A  # the individual ID
-  pos::A  # the individuals position
+  pos  # the individuals position
   species::A  # the species ID the individual belongs to
   y::B  # a vector that specifies the importance of each trait
   W::D  # fitness. W = exp(γ .* transpose(z .- θ)*inv(ω)*(z .- θ)). # z::C  # the phenotype vector. z = By .+ μ
@@ -22,56 +22,68 @@ Innitializes the model.
 * P a tuple  specifying the number of traits p for each species
 * B  [Random.bitrand(i[1], i[2]) for i in zip(P, L)]  a tuple  of pleiotropy matrices, one for  each species. Each matrix consists of zeros and ones only. make sure no rows are all zero (a trait * is not controled by any locus)
 * γ [-0.5, -0.5] a tuple  of selection coefficients for each species
-* m ploidy, m=2 diploid
+* m (2, 1) ploidy for each species.
 * T [randn(Float16, n) for n in P]  a tuple  of arrays, each θ specifying optimal phenotypes for  each species
 * Ω [Symmetric(rand(Float16, i[1], i[2])) for i in zip(P, P)] a tuple  of matrices, each of which ω represents a covariance matrix of the selection surface
 * M [0.02, 0.02] A tuple of mutation rates μ for each species
 * MB [0.05, 0.05] A tuple of mutation rates μ<sub>B</sub> for each species
-* N [1000, 1000] a tuple  for population size of each species
+* N  Dict(1 => (1000, 1000)), a dictionary where a key is node number and the value is a tuple for population size of each species at that node
 * Y [rand(Float16, i*m) for i in L] a tuple  of Arrays, each specifying the initial y vector of  each species
 * E [0.8, 0.8] a tuple  of the variance of a normal distribution ε representing environmental * noise for each species.
 * generations 100  number of generations to run the simulation
+* space (2,2)  Either a tuple for a grid size or a SimpleGraph
+* node_capacities Dict(1 => 2000) a dictionary where a key is node number and a value is an integer for carrying capacity of the node
+* migration_rates [1.0 0.02 0.02 0.02; 0.03 1.0 0.03 0.03; 0.01 0.01 1.0 0.01; 0.01 0.01 0.01 1.0] a matrix of migration rates between each pair of nodes. The rows and columns of the matrix are node numbers in order.
+* moore false whether diagonal nodes in a grid should be connected.
 """
-function model_initiation(;L, P, B, γ, m, T, Ω, M, MB, N, Y, E, generations, space=nothing, periodic=false, moore=false, seed=0)
+function model_initiation(;L, P, B, γ, m, T, Ω, M, MB, N, Y, E, generations, migration_rates, node_capacities, space=nothing, periodic=false, moore=false, seed=0)
   if seed >0
     Random.seed!(seed)
   end
-
-  # Some checks for parameters having the correct dimensions
-  @assert m < 3  "Ploidy more than 2 is not implemented"
-  @assert length.(Y) .% m == Tuple([0 for i in 1:length(Y)]) "Length of elements in Y are not correct. They should a factor of m"
-  @assert size.(B, 2) .% m == Tuple([0 for i in 1:length(Y)]) "Length of elements in B are not correct. They should a factor of m"
-  @assert length(γ) == length(N) == length(L) == length(B) == length(T) == length(MB) == length(M) == length(Y) == length(E) == length(P) == length(Ω) "L, B, N, γ, T, MB, M, P, Y, Ω, and E should have the same number of elements"
-
+  
   if space == nothing
-    fspace = Space((1, 1), periodic=periodic, moore=moore)
+    fspace = Space((1, 1))
   elseif typeof(space) <: NTuple
     fspace = Space(space, periodic=periodic, moore=moore)
   elseif typeof(space) <: AbstractGraph
     fspace = Space(space)
+  end
+  nspecies = length(L)
+
+  # Some checks for parameters having the correct dimensions
+  for i in m
+    @assert i < 3  "Ploidy more than 2 is not implemented"
+  end
+  @assert length.(Y) .% m == Tuple([0 for i in 1:length(Y)]) "Length of elements in Y are not correct. They should a factor of m"
+  @assert size.(B, 2) .% m == Tuple([0 for i in 1:length(Y)]) "Length of elements in B are not correct. They should a factor of m"
+  @assert length(γ) == length(L) == length(B) == length(T) == length(MB) == length(M) == length(Y) == length(E) == length(P) == length(Ω) "L, B, γ, T, MB, M, P, Y, Ω, and E should have the same number of elements"
+  @assert length(keys(node_capacities)) >= nv(fspace) "node_capacities should have a key for every node"
+  for (k, v) in N
+    @assert length(v) == nspecies "Each value in N should have size equal to number of species"
+  end
+  if typeof(migration_rates) <: AbstractArray
+    @assert size(migration_rates, 1) == nv(fspace) "migration_rates has more rows than there are nodes in space."
   end
 
   Ed = [Normal(0.0, i) for i in E]
   # A descrete non-parametric distribtion of uB for each species
   dnps = [DiscreteNonParametric([true, false], [i, 1-i]) for i in MB]
   Mdists = [Normal(0, i) for i in M]
-
-  properties = Dict(:L => L, :P => P, :B => B, :γ => γ, :m => m, :T => T, :Ω => inv.(Ω), :M => Mdists, :MB => dnps, :N => N, :Y => Y, :E => Ed, :generations => generations, :node_capacities => node_capacities)
+  
+  properties = Dict(:L => L, :P => P, :B => B, :γ => γ, :m => m, :T => T, :Ω => inv.(Ω), :M => Mdists, :MB => dnps, :N => N, :Y => Y, :E => Ed, :generations => generations, :node_capacities => node_capacities, :migration_rates => migration_rates, :nspecies => nspecies)
   model = ABM(Ind, fspace, properties=properties)
   # create and add agents
-  indcounter = 0
-  for (sind, n) in enumerate(properties[:N])
-    x = properties[:B][sind]*properties[:Y][sind]
-    d = properties[:E][sind]
-    for ind in 1:n
-      indcounter += 1
-      z = x .+ rand(d)
-      takeabs = abs.(z .- properties[:T][sind])
-      # W = exp(γ[sind] * transpose(takeabs)* Ω[sind] *takeabs)
-      W = exp(properties[:γ][sind] * transpose(takeabs)*properties[:Ω][sind]*takeabs)
-      W = minimum([1e5, W])
-      individual = Ind(indcounter, sind, deepcopy(properties[:Y][sind]), W, deepcopy(properties[:B][sind]))
-      model.agents[indcounter] = individual
+  for (pos, Ns) in properties[:N]
+    for (sind, n) in enumerate(Ns)
+      x = properties[:B][sind]*properties[:Y][sind]
+      d = properties[:E][sind]
+      for ind in 1:n
+        z = x .+ rand(d)
+        takeabs = abs.(z .- properties[:T][sind])
+        W = exp(properties[:γ][sind] * transpose(takeabs)*properties[:Ω][sind]*takeabs)
+        W = minimum([1e5, W])
+        add_agent!(pos, model, sind, deepcopy(properties[:Y][sind]), W, deepcopy(properties[:B][sind]))
+      end
     end
   end
 
@@ -84,7 +96,7 @@ end
 A function to define what happens within each step of the model.
 """
 function model_step!(model::ABM)
-  if model.properties[:m] == 2
+  if sum(model.properties[:m]) > length(model.properties[:m]) # there is at least one diploid
     sexual_reproduction!(model)
   end
   selection!(model)
@@ -93,17 +105,17 @@ end
 function agent_step!(agent::Ind, model::ABM)
   mutation!(agent, model)
   update_fitness!(agent, model)
-  # TODO migration
+  migration!(agent, model)
 end
 
 function selection!(model::ABM)
   for node in 1:nv(model)
-    sample!(model, model.properties[:node_capacities][node], node, :W)  # TODO: think about node_capacities format. It should ideally allow different K for different species.
+    sample!(model, model.properties[:node_capacities][node], node, :W)
   end
 end
 
 """
-Choose a random mate and produce 2 offsprings with recombination.
+Choose a random mate and produce one offsprings with recombination.
 """
 function sexual_reproduction!(model::ABM, node_number::Int)
   node_content = get_node_contents(node_number, model)
@@ -119,7 +131,7 @@ function sexual_reproduction!(model::ABM, node_number::Int)
 end
 
 function sexual_reproduction!(model::ABM)
-  for node in 1:nv
+  for node in 1:nv(model)
     sexual_reproduction!(model, node)
   end
 end
@@ -127,7 +139,7 @@ end
 "Returns an array of tuples for each pair of agent ids to reproduce"
 function mate(model::ABM, node_number::Int)
   node_content = get_node_contents(node_number, model)
-  same_species = [[k for k in node_content if model.agents[k].species == i] for i in 1:length(model.properties[:N])]
+  same_species = [[k for k in node_content if model.agents[k].species == i] for i in 1:model.properties[:nspecies] if model.properties[:m][i] == 2]
 
   mates = Array{Tuple{Int, Int}}(undef, sum(length.(same_species)))
 
@@ -194,6 +206,20 @@ function update_fitness!(model::ABM)
   end
 end
 
+"Move the agent to a new node with probabilities given in :migration_rates"
+function migration!(agent::Ind, model::ABM)
+  if model.properties[:migration_rates] == nothing
+    return
+  end
+  vertexpos = coord2vertex(agent.pos, model)
+  row = model.properties[:migration_rates][vertexpos, :]
+  new_node = sample(1:length(row), Weights(row))
+  if new_node != vertexpos
+    move_agent!(agent, new_node, model)
+  end
+end
+
+"Replace Inds in a node with a weighted by :W sample of Inds with replacement"
 function sample!(model::ABM, n::Int, node_number::Int, weight=nothing; replace=true,
   rng::AbstractRNG=Random.GLOBAL_RNG)
 
@@ -206,9 +232,12 @@ function sample!(model::ABM, n::Int, node_number::Int, weight=nothing; replace=t
       newids = sample(rng, node_content, n, replace=replace)
   end
 
-  for id in newids # add new agents while adjusting id
-    model.agents[max_id + 1] = deepcopy(model.agents[id])
-    push!(model.space.agent_positions[coord2vertex(model.agents[id].pos, model)], max_id + 1)
+  for id in newids # add new agents to the model
+    ag = deepcopy(model.agents[id])
+    ag.id = max_id + 1
+    add_agent_pos!(ag, model)
+    # model.agents[max_id + 1] = deepcopy(model.agents[id])
+    # push!(model.space.agent_positions[coord2vertex(model.agents[id].pos, model)], max_id + 1)
     max_id += 1
   end
 
