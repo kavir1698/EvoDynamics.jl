ed2d0be73755d1356f6033672981760695716e3a
Ali R. Vahdati <arv.kavir@gmail.com>
2020-04-07 15:48:19 +0200

    Fix tests

--- test/api_tests.jl
+++ test/api_tests.jl
@@ -2,13 +2,13 @@ Random.seed!(10)
 
 @testset "Model" begin
   parameters2 = deepcopy(parameters)
-  a = runmodel(parameters2)
+  a, model = runmodel(parameters2)
   @test size(a, 1) == parameters2[:generations] + 1
   @test maximum(a.step) == parameters2[:generations]
   
   parameters2[:space] = nothing
   parameters2[:migration_rates] = [nothing, nothing]
-  a = runmodel(parameters2)
+  a, model = runmodel(parameters2)
   @test size(a, 1) == parameters2[:generations] + 1
   @test maximum(a.step) == parameters2[:generations]
 end
