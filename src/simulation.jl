9240635c5646b8d5c89da892f5d16c958f3022e6
Ali R. Vahdati <arv.kavir@gmail.com>
2020-04-14 09:38:24 +0200

    fix bug

--- src/simulation.jl
+++ src/simulation.jl
@@ -72,8 +72,11 @@ function model_initiation(;ngenes, nphenotypes, epistasisMat, pleiotropyMat, exp
   end
 
   properties = Dict(:ngenes => ngenes, :nphenotypes => nphenotypes, :epistasisMat => newA, :pleiotropyMat => pleiotropyMat, :expressionArrays => newQ, :growthrates => growthrates, :competitionCoeffs => competitionCoeffs, :selectionCoeffs => selectionCoeffs, :ploidy => ploidy, :optPhenotypes => optPhenotypes, :covMat => inv.(newcovMat), :mutProbs => Mdists, :mutMagnitudes => Ddists, :N => N, :E => Ed, :generations => generations, :K => K, :migration_rates => migration_rates, :nspecies => nspecies)
-  tempInd = Ind(1, (1,1), 1, 0.1, properties[:epistasisMat][1], properties[:pleiotropyMat][1], properties[:expressionArrays][1])
-  model = ABM(typeof(tempInd), fspace, properties=properties)
+
+  indtype = EvoDynamics.Ind{typeof(0.1), eltype(properties[:epistasisMat]), eltype(properties[:pleiotropyMat]), eltype(properties[:expressionArrays])}
+EvoDynamics.Ind{Float64,Array{Float64,2},Array{Bool,2},Array{Float64,1}}
+  model = ABM(indtype, fspace, properties=properties)
+  
   # create and add agents
   for (pos, Ns) in properties[:N]
     for (ind2, n) in enumerate(Ns)
