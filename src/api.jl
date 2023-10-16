d905d2d5a673a166142ebd8d47bcd7973f69e608
Ali R. Vahdati <3798865+kavir1698@users.noreply.github.com>
2023-10-16 22:59:39 +0200

    api bug fix

--- src/api.jl
+++ src/api.jl
@@ -41,7 +41,8 @@ Creates and runs a model given `parameters`. Returns a `DataFrame` of collected 
 
 * adata=[] agent data to be collected. Either agent fields or functions that accept an agent as input can be put in the array. To aggregate collected data, provide tuples inside the array. For example, to collect mean and median fitness of individuals which is in field `W`, your array will be [(:W,mean), (:W,median)].
 * mdata=[mean_fitness_per_species, species_N] model data to be collected. By default, collects mean population fitness per species. Each row of the output DataFrame corresponds to all agents and each column is the value function applied to a field. The functions in the array are applied to the model object.
-* when=nothing The generations from which data are collected. By default collect at all steps.
+* when_data=nothing The generations from which agent data are collected. By default collect at all steps.
+* when_model=nothing The generations from which model data are collected. By default collect at all steps.
 * replicates::Int = 0 Number of replicates per simulation.
 * parallel::Bool = false Whether to run replicates in parallel. If `true`, you should add processors to your julia session (e.g. by `addprocs(n)`) and define your parameters and `EvoDynamics` on all workers. To do that, add `@everywhere` before them. For example, `@everywhere EvoDynamics`.
 * seeds = optionally, provide an array of integers as seeds for each replicate.
@@ -56,7 +57,8 @@ Creates and runs a model given `parameters`. Returns a `DataFrame` of collected 
 """
 function runmodel(param_file::AbstractString;
   adata=nothing, mdata=[mean_fitness_per_species, species_N],
-  when=nothing,
+  when_data=nothing,
+  when_model=nothing,
   replicates::Int=0,
   parallel::Bool=false,
   seeds=nothing,
@@ -76,26 +78,28 @@ function runmodel(param_file::AbstractString;
   @info "Loading the parameters..."
   dd = load_parameters(param_file)
 
-  if isnothing(when)
-    whenn = 0:dd[:generations]
-  else
-    whenn = when
+  if isnothing(when_data)
+    when_data = 0:dd[:generations]
   end
+  if isnothing(when_model)
+    when_model = 0:dd[:generations]
+  end
+
   # create model
   @info "Running the model..."
   if replicates == 0
     model = model_initiation(dd)
     if offline_run
-      offline_run!(model, agentstep, modelstep, model.generations, adata=adata, mdata=mdata, when=whenn, agents_first=false, showprogress=showprogress, adata_filename=adata_filename, mdata_filename=mdata_filename,writing_interval=writing_interval)
+      offline_run!(model, agentstep, modelstep, model.generations, adata=adata, mdata=mdata, when=when_data, when_model=when_model, agents_first=false, showprogress=showprogress, adata_filename=adata_filename, mdata_filename=mdata_filename,writing_interval=writing_interval)
     else
       # run model and collect data
-      agdata, modata = run!(model, agentstep, modelstep, model.generations, adata=adata, mdata=mdata, when=whenn, agents_first=false, showprogress=showprogress)
+      agdata, modata = run!(model, agentstep, modelstep, model.generations, adata=adata, mdata=mdata, when=when_data, when_model=when_model, agents_first=false, showprogress=showprogress)
       return agdata, modata, [model]
     end
   else
     models = [model_generator(i, seeds, param_file) for i in 1:replicates]
 
-    agdata, modata, models = ensemblerun!(models, agentstep, modelstep, dd[:generations], adata=adata, mdata=mdata, when=whenn, parallel=parallel, agents_first=false, showprogress=showprogress)
+    agdata, modata, models = ensemblerun!(models, agentstep, modelstep, dd[:generations], adata=adata, mdata=mdata, when=when_data, when_model=when_model, parallel=parallel, agents_first=false, showprogress=showprogress)
     return agdata, modata, models
   end
 end
